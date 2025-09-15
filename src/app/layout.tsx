8fb0db1f00bb98cd8ff500e50fa54ca6f63fd02f
fixbro.in <djdjapps@gmail.com>
2025-09-15 14:22:59 +0530

    Initial clean commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -92,6 +92,18 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "tb2fkr70df");
+            `,
+          }}
+        />
       </head>
       <body className={cn('font-body antialiased')}>
         <ThemeProvider
